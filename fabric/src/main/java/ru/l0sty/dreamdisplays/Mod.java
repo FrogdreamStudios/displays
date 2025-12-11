5ac13a32fdd244e26466901e9955eea82b092bbb
Arsenii Motorin <74359983+arsmotorin@users.noreply.github.com>
2025-12-11 01:19:21 +0100

    Revert "1.1.0"

---
+++ fabric/src/main/java/ru/l0sty/dreamdisplays/Mod.java
@@ -0,0 +1,10 @@
+package ru.l0sty.dreamdisplays;
+
+import net.minecraft.network.packet.CustomPayload;
+
+/**
+ * Interface for mods that can send packets.
+ */
+public interface Mod {
+    void sendPacket(CustomPayload packet);
+}
--- spigot/src/main/java/com/dreamdisplays/scheduler/Adapter.kt
+++
@@ -1,9 +0,0 @@
-package com.dreamdisplays.scheduler
-
-import org.bukkit.plugin.Plugin
-import org.jspecify.annotations.NullMarked
-
-@NullMarked
-interface Adapter {
-    fun runRepeatingAsync(plugin: Plugin, delayTicks: Long, intervalTicks: Long, task: Runnable)
-}
