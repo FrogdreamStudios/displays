9a4329604885e5aef689842bd485174e9bc39b69
Arsenii <cubelius@icloud.com>
2026-08-28 22:24:52 +0200

    fix: fix storage manager logger

--- platform/server/src/main/kotlin/com/dreamdisplays/platform/server/managers/StorageManager.kt
+++ platform/server/src/main/kotlin/com/dreamdisplays/platform/server/managers/StorageManager.kt
@@ -114,8 +114,8 @@ class StorageManager(
     username: String = "",
     password: String = "",
     useSSL: Boolean = false,
-    private val logger: Logger = LoggerFactory.getLogger(javaClass),
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
     private val table = DisplaysTable(tablePrefix)
 
     private val dataSource = HikariDataSource(HikariConfig().apply {
