39ae142aa69bf01abc44584dfb419481e6429df8
Arsenii <cubelius@icloud.com>
2026-09-01 17:06:07 +0200

    fix(client): fullscreen videos looping forever even without the looped flag

--- media/player/src/main/kotlin/com/dreamdisplays/media/player/MediaPlayer.kt
+++ media/player/src/main/kotlin/com/dreamdisplays/media/player/MediaPlayer.kt
@@ -776,7 +776,11 @@ class MediaPlayer(
         }
 
         if (normalEos && !liveStream) {
-            restartFromBeginning()
+            if (host.shouldLoopOnEnd) {
+                restartFromBeginning()
+            } else {
+                host.onPlaybackEnded(durationHintNanos)
+            }
             return
         }
 
